f2e21a67a887397ba3ac58ef8bf517c56f135418
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-08-11 23:55:57 -0300

    creating more complex views

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blogposts/', views.BlogPostListCreate.as_view(), name="blogpost-view-create")
+    path(
+        'blogposts/', 
+        views.BlogPostListCreate.as_view(), 
+        name="blogpost-view-create"
+    ),
+    path(
+        'blogposts/<int:pk>/', 
+        views.BlogPostRetrieveUpdateDestroy.as_view(),
+        name="blogpost-view-update"
+    ),
+    
 ]
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import BlogPost
 from .serializers import BlogPostSerializer
 
@@ -7,4 +7,13 @@ from .serializers import BlogPostSerializer
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+    
+    def delete(self, request, *args, **kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = "pk"
     
